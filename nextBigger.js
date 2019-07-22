7d11427f82ea5e7944bafb55697b4c6489f3707f
Cullan Shewfelt <cullanrocks@gmail.com>
2019-07-22 14:40:18 -0700

    nextBigger.js'

---
+++ nextBigger.js
@@ -0,0 +1,13 @@
+function nextBigger(n){
+  let nArray = n.toString().split('');
+  let combos = [];
+  nArray.reduce((acc, curr, i, array) => {
+    console.log(5, i)
+    console.log(6, array)
+    return acc.
+  }, [])
+  return combos;
+}
+
+
+console.log(30, nextBigger(2017))
