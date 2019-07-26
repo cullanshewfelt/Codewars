b849d28f6034def1a0436af9f11f2cfc46ede25b
Cullan Shewfelt <cullanrocks@gmail.com>
2019-07-26 12:15:50 -0700

    nextBigger.js

--- nextBigger.js
+++ nextBigger.js
@@ -1,13 +1,43 @@
 function nextBigger(n){
+  let pivot = -1;
   let nArray = n.toString().split('');
-  let combos = [];
-  nArray.reduce((acc, curr, i, array) => {
-    console.log(5, i)
-    console.log(6, array)
-    return acc.
-  }, [])
-  return combos;
+
+  for(let x = nArray.length - 1; x >= 0; x--){
+    if(+nArray[x] > +nArray[x - 1]){
+      pivot = x - 1;
+      break;
+    }
+  }
+  if(pivot === -1){
+    return pivot;
+  }
+
+  let right = nArray.splice(pivot);
+
+  pivot = right.splice(0, 1)[0];
+
+  let nextBiggest = null;
+  let nextBiggestIndex = null;
+
+  for (let y = 0; y < right.length; y++) {
+    if (right[y] > pivot) {
+      if (nextBiggest == null || right[y] < nextBiggest) {
+        nextBiggest = right[y];
+        nextBiggestIndex = y;
+      }
+    }
+  }
+
+  if (nextBiggestIndex == null) return -1;
+
+  right.splice(nextBiggestIndex, 1);
+  right.push(pivot);
+  right = right.sort();
+
+  var newNumber = +nArray.concat([nextBiggest]).concat(right).join('');
+  if (newNumber < n) return -1;
+  return newNumber;
 }
 
 
-console.log(30, nextBigger(2017))
+nextBigger(2017)
