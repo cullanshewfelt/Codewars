d35de798d3d1b88d197e9cd2558f260ee06f4eaa
Cullan Shewfelt <cullanrocks@gmail.com>
2019-07-29 19:57:06 -0700

    working on sum of divided.

--- sumOfDivided.js
+++ sumOfDivided.js
@@ -1,35 +1,49 @@
 function sumOfDivided(lst) {
-  let factors = [];
   let allFactors = [];
 
   const isPrime = num => {
     for(let i = 2; i < num; i++){
       if(num % i === 0){
-        return false;
+        return false
       }
     }
-    return num > 1;
+    return num > 1
   }
 
-  const refactor = numb => {
-    let primes = [];
-    for(let i = 2; i < numb; i++){
-      if(numb % i === 0){
-        primes.push(i)
+  const refactor = num => {
+    let factors = [];
+    for(let i = 2; i <= num; i++){
+      if(num % i === 0){
+        isPrime(i) && factors.push(i)
       }
     }
+    return factors;
   }
 
+  allFactors = lst.map(number => ({
+    total: number,
+    factors: refactor(number)
+  }));
 
-  lst.forEach(number => {
-    factors = [];
-    let y = isPrime(number) ? number :
-    console.log(15, y)
-    allFactors.push(y);
-  });
-  console.log(25, allFactors)
+  let primes = new Set(allFactors.reduce((acc, curr) => {
+    return [...acc, ...curr.factors]
+  }, []))
+
+  primes = [...primes].sort((x, y) => x - y);
+
+  const totalReducer = (currentPrime) => {
+    return lst.reduce((acc, curr) => {
+      return acc += curr % currentPrime === 0 ? curr : 0
+    }, 0)
+  }
+
+  let answer = primes.reduce((acc, curr) => {
+    return [...acc, [curr, totalReducer(curr)]]
+  }, [])
+
+  return answer
 }
 
-let answer = sumOfDivided([15,21,24,30,45]);
+let x = sumOfDivided([1070, 1580, 2040, 1000, 1180, 1230, 1260, 1100, 1160, 1000]);
 
-// console.log(answer)
+console.log(x)
